98dc5c4456b2d16df45da27af758853044dc7b69
ZhiLeong <saber_zhileong@hotmail.com>
2023-01-10 22:35:30 -0500

    commit tree 95e1744833c938b1f813b14e9c39cd55dfcb0cd8

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
 
-end
+    
+    def name=(name)
+        @name = name
+    end
+    def breed=(breed)
+        @breed = breed
+    end
+    
+    def name
+        @name
+    end
+    def breed
+        @breed
+    end
+  
+  end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
 
-end
+    
+    def name=(name)
+        @name = name
+    end
+    def job=(job)
+        @job = job
+    end
+    
+    def name
+        @name
+    end
+    def job
+        @job
+    end
+  
+  end
